ebecc0f4133a669f4414293cd4d68968023e7127
maysqunaibi <maysqunaibi2000@gmail.com>
2021-05-25 23:53:30 +0300

    change the workdir name

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://mongo:27017/cinema', { useNewUrlParser: true })
+    .connect('mongodb://mongo:27017/blog', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
